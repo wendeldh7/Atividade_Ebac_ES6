1e3806079b58e2c64f39f2e1b2e4554d6a2524f5
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-04-24 12:05:04 -0400

    Primeiro commit

--- src/main.js
+++ src/main.js
@@ -1,30 +1,15 @@
 // Exercício Nota Escolar
 // Obter a média a partir de uma array
 
-// 0-5 = F
-// 6-7 = D
-// 7-8 = C
-// 8-9 = B
-// 9-10 = A
-const array = [6, 7,8];
-// Média 6
+const alunos = ['Jose', 'Maria', 'Gustavo'];
 
-console.log(mediaDoAluno(array));
-
-function mediaDoAluno(notas) {
-        const media = calcularMedia(notas);
-
-    if (media < 5) return 'F';
-    if (media < 6) return 'D';
-    if (media < 7) return 'C';
-    if (media < 8) return 'B';
-    return 'A'
-}
-
-function calcularMedia(array) {
-    const soma = 0;
-    for (let valor of array) {
-        soma += valor;
+const alunos2 = alunos.map(function(itemAtual) {
+    return {
+        nome: itemAtual,
+        curso: 'Frontend'
     }
-    return soma/(array.length);
-}
+})
+
+console.log(alunos2);
+
+Video aula 20:38,
