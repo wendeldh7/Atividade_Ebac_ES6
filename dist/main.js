173a2555a0a1382086f07bbc29dee7ccaa3d9395
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-04-26 08:15:51 -0400

    Primeiro commit

--- dist/main.js
+++ dist/main.js
@@ -3,11 +3,15 @@
 // Exercício Nota Escolar
 // Obter a média a partir de uma array
 
-var alunos = ['Jose', 'Maria', 'Gustavo'];
-var alunos2 = alunos.map(function (itemAtual) {
-  return {
-    nome: itemAtual,
-    curso: 'Frontend'
-  };
+var listaPresenca = ['Ana', 'José', 'João', 'Bianca'];
+var notas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(listaPresenca[0]);
+var lista1 = ['Ana', 'José', 'João', 'Bianca'];
+var lista2 = ['Predo', 'Joaquim', 'Maria', 'Olivia'];
+var listas = lista1.concat(lista2);
+console.log(listas);
+var notas1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var notasBoas = notas1.filter(function (n) {
+  return n >= 6;
 });
-console.log(alunos2);
+console.log(notasBoas);
--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 // Exercício Nota Escolar
 // Obter a média a partir de uma array
 
-const alunos = ['Jose', 'Maria', 'Gustavo'];
+const listaPresenca = ['Ana', 'José', 'João', 'Bianca']
+const notas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const alunos2 = alunos.map(function(itemAtual) {
-    return {
-        nome: itemAtual,
-        curso: 'Frontend'
-    }
-})
+console.log(listaPresenca[0]);
 
-console.log(alunos2);
+const lista1 = ['Ana', 'José', 'João', 'Bianca']
+const lista2 = ['Predo', 'Joaquim', 'Maria', 'Olivia']
 
-Video aula 20:38,
+const listas = lista1.concat(lista2)
+
+console.log(listas);
+
+const notas1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const notasBoas = notas1.filter(n => n >= 6)
+
+console.log(notasBoas);
